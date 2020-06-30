90395e474a707e8278015aba0504b4aa08422c7b
yamp <coramdeo0708@gmail.com>
2020-07-01 05:55:59 +0900

    update for recent status

---
+++ LSP/template.c
@@ -0,0 +1,23 @@
+/**
+*	@file			
+*	@manual		
+*	@brief		
+*	@reference	
+*	@functions	
+*
+*	@author		yamp
+*	@date		Apr 26, 2020
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv) {
+
+	return 0;
+}
